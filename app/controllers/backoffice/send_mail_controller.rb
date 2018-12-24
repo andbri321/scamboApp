988a78d820c154fdde87ad2a1ae0fa9bd4211779
andbri <andbri321@gmail.com>
2018-12-24 18:25:32 -0300

    usando modal com ajax para enviar e-mail

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -6,7 +6,20 @@ class Backoffice::SendMailController < ApplicationController
 		end 
 	end
 
+	#11 min
 	def create
-		
+		begin
+			AdminMailer.send_message(current_admin,params[:'recipient-text'],params[:'subject-text'],params[:'message-text'])
+			@notify_message = "Email enviado com sucesso !"
+			@notify_flag = "success"
+		rescue
+			@notify_message = "Erro ao enviar email. Tente novamente !"
+			@notify_flag = "error"
+		end
+
+		respond_to do |format|
+			format.js
+		end
 	end
+
 end
